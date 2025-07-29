8c42ec1df7bdbd5ce7496f5a1a68457b3ae9ff27
kazohobana <Kazohoba@gmail.com>
2025-07-29 12:49:26 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Assuming App.jsx is in the same folder
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
